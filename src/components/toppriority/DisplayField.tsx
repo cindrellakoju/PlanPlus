e923b48b7d8fe3b2ed61f2ec131f8804fa94423b
cindrella koju <cindrellakoju@gmail.com>
2025-02-13 17:24:41 +0545

    create require file for top priority

---
+++ src/components/toppriority/DisplayField.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const DisplayField:React.FC = () =>{
+    return(
+        <div className="displayfieldcontainer">
+            <div className="smallcircle">
+                <p></p>
+            </div>
+        </div>
+    )
+}
+
+export default DisplayField;
---
+++ src/components/toppriority/TopPriority.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const TopPriority:React.FC = () =>{
+    return(
+        <div className="prioritycontainer">
+        </div>
+    )
+}
+
+export default TopPriority;
