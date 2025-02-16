6e576db14ac71a5e57eb6dcff428cde5a4f94881
cindrella koju <cindrellakoju@gmail.com>
2025-02-16 14:05:32 +0545

    done html css of bucketlist

--- src/components/bucketlist/BucketList.tsx
+++ src/components/bucketlist/BucketList.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import BucketListField from "./BucketListField";
+import "../../styles/BucketList.css"
 
 const BucketList:React.FC = () =>{
     return(
-        <div>
+        <div className="bucketlistcontainer">
             <h1>Bucket List</h1>
             <BucketListField/>
         </div>
--- src/components/bucketlist/BucketListField.tsx
+++ src/components/bucketlist/BucketListField.tsx
@@ -1,9 +1,52 @@
 import React from "react";
+import InputField from "../allrequire/InputField";
 
 const BucketListField:React.FC = () =>{
     return(
-        <div>
-            
+        <div className="bucketlistfieldcontainer">
+            <div className="bucketlistfields">
+                <div className="field">
+                    <input type="checkbox" id="bucketlist1" name="bucketlist1"  value="bucketlist" />
+                    <label htmlFor="bucketlist1">To visit AnnapurnaBase Camp </label>                    
+                </div>
+                <div className="field">
+                    <input type="checkbox" id="bucketlist1" name="bucketlist1"  value="bucketlist" />
+                    <label htmlFor="bucketlist1">To visit AnnapurnaBase Camp </label>                    
+                </div>
+                <div className="field">
+                    <input type="checkbox" id="bucketlist1" name="bucketlist1"  value="bucketlist" />
+                    <label htmlFor="bucketlist1">To visit AnnapurnaBase Camp  sfds fs dfbsfkbsdf fbb f dbsfbksf sdfsdfbsdf sb fbsf s fsbfiudsf s </label>                    
+                </div>
+                <div className="field">
+                    <input type="checkbox" id="bucketlist1" name="bucketlist1"  value="bucketlist" />
+                    <label htmlFor="bucketlist1">To visit AnnapurnaBase Camp </label>                    
+                </div>
+                <div className="field">
+                    <input type="checkbox" id="bucketlist1" name="bucketlist1"  value="bucketlist" />
+                    <label htmlFor="bucketlist1">To visit AnnapurnaBase Camp </label>                    
+                </div>
+                <div className="field">
+                    <input type="checkbox" id="bucketlist1" name="bucketlist1"  value="bucketlist" />
+                    <label htmlFor="bucketlist1">To visit AnnapurnaBase Camp </label>                    
+                </div>
+                <div className="field">
+                    <input type="checkbox" id="bucketlist1" name="bucketlist1"  value="bucketlist" />
+                    <label htmlFor="bucketlist1">To visit AnnapurnaBase Camp </label>                    
+                </div>
+                <div className="field">
+                    <input type="checkbox" id="bucketlist1" name="bucketlist1"  value="bucketlist" />
+                    <label htmlFor="bucketlist1">To visit AnnapurnaBase Camp </label>                    
+                </div>
+                <div className="field">
+                    <input type="checkbox" id="bucketlist1" name="bucketlist1"  value="bucketlist" />
+                    <label htmlFor="bucketlist1">To visit AnnapurnaBase Camp </label>                    
+                </div>
+                <div className="field">
+                    <input type="checkbox" id="bucketlist1" name="bucketlist1"  value="bucketlist" />
+                    <label htmlFor="bucketlist1">To visit AnnapurnaBase Camp </label>                    
+                </div>
+            </div>
+            <InputField/>
         </div>
     )
 }
