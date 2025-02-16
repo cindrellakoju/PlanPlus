89921e5d942e19ea0c2cebf1d3e2331bea7e9208
cindrella koju <cindrellakoju@gmail.com>
2025-02-16 14:21:44 +0545

    schedu;e

--- src/components/schdedule/Schedule.tsx
+++ src/components/schdedule/Schedule.tsx
@@ -1,10 +1,10 @@
 import React from "react"
-import { today_day } from "../../utils/formatDate"
+import { today_day_long } from "../../utils/formatDate"
 
 const Schedule:React.FC= () =>{
     return(
         <div className="schedulecontainer">
-            <h1>Schedule for {today_day}</h1>
+            <h1>Schedule for {today_day_long}</h1>
         </div>
     )
 }
---
+++ src/components/schdedule/ScheduleField.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ScheduleField:React.FC = () =>{
+    return(
+        <>
+        </>
+    )
+}
+
+export default ScheduleField;
