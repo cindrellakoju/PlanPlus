73c3530fb4b271253e58ae7b163a7332b18875c2
cindrella koju <cindrellakoju@gmail.com>
2025-02-16 14:29:20 +0545

    adde input field

--- src/components/toppriority/DisplayField.tsx
+++ src/components/toppriority/DisplayField.tsx
@@ -1,46 +1,49 @@
 import React from "react";
+import InputField from "../allrequire/InputField";
 
 const DisplayField:React.FC = () =>{
     return(
-        <>
         <div className="displayfieldcontainer">
-            <div className="smallcircle"></div>
-            <p>To complete the top priority field</p>
+            <div className="displayfields">
+                <div className="field">
+                    <div className="smallcircle"></div>
+                    <p>To complete the top priority field</p>
+                </div>
+                <div className="field">
+                    <div className="smallcircle"></div>
+                    <p>To complete the top priority field</p>
+                </div>
+                <div className="field">
+                    <div className="smallcircle"></div>
+                    <p>To complete the top priority field</p>
+                </div>
+                <div className="field">
+                    <div className="smallcircle"></div>
+                    <p>To complete the top priority field</p>
+                </div>
+                <div className="field">
+                    <div className="smallcircle"></div>
+                    <p>To complete the top priority field</p>
+                </div>
+                <div className="field">
+                    <div className="smallcircle"></div>
+                    <p>To complete the top priority field</p>
+                </div>
+                <div className="field">
+                    <div className="smallcircle"></div>
+                    <p>To complete the top priority field</p>
+                </div>
+                <div className="field">
+                    <div className="smallcircle"></div>
+                    <p>To complete the top priority field</p>
+                </div>
+                <div className="field">
+                    <div className="smallcircle"></div>
+                    <p>To complete the top priority field</p>
+                </div>
+            </div>
+            <InputField/>
         </div>
-        <div className="displayfieldcontainer">
-            <div className="smallcircle"></div>
-            <p>To complete the top priority field</p>
-        </div>
-        <div className="displayfieldcontainer">
-            <div className="smallcircle"></div>
-            <p>To complete the top priority field</p>
-        </div>
-        <div className="displayfieldcontainer">
-            <div className="smallcircle"></div>
-            <p>To complete the top priority field</p>
-        </div>
-        <div className="displayfieldcontainer">
-            <div className="smallcircle"></div>
-            <p>To complete the top priority field</p>
-        </div>
-        <div className="displayfieldcontainer">
-            <div className="smallcircle"></div>
-            <p>To complete the top priority field</p>
-        </div>
-        <div className="displayfieldcontainer">
-            <div className="smallcircle"></div>
-            <p>To complete the top priority field</p>
-        </div>
-        <div className="displayfieldcontainer">
-            <div className="smallcircle"></div>
-            <p>To complete the top priority field</p>
-        </div>
-        <div className="displayfieldcontainer">
-            <div className="smallcircle"></div>
-            <p>To complete the top priority field</p>
-        </div>
-
-        </>
     )
 }
 
