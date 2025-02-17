cc7baf2c0d8ea460472326ec820b660c438bcdfb
cindrella koju <cindrellakoju@gmail.com>
2025-02-17 15:10:47 +0545

    base for schedule done

--- src/components/schdedule/Schedule.tsx
+++ src/components/schdedule/Schedule.tsx
@@ -1,10 +1,13 @@
 import React from "react"
 import { today_day_long } from "../../utils/formatDate"
+import ScheduleField from "./ScheduleField"
+import "../../styles/Schedule.css"
 
 const Schedule:React.FC= () =>{
     return(
         <div className="schedulecontainer">
             <h1>Schedule for {today_day_long}</h1>
+            <ScheduleField/>
         </div>
     )
 }
--- src/components/schdedule/ScheduleField.tsx
+++ src/components/schdedule/ScheduleField.tsx
@@ -1,10 +1,59 @@
 import React from "react";
+import "../../styles/Schedule.css";
 
-const ScheduleField:React.FC = () =>{
-    return(
-        <>
-        </>
-    )
-}
+const ScheduleField: React.FC = () => {
+    return (
+        <div className="schedulefield">
+            <div className="fields">
+                <p className="timeslot">8.00 am</p>
+                <p className="infoslot">
+                    Lorem ipsum dolor, sit amet consectetur adip
+                </p>
+            </div>
+            <div className="fields">
+                <p className="timeslot">8.00 am</p>
+                <p className="infoslot">
+                    Lorem ipsum dolor, sit ame
+                </p>
+            </div>
+            <div className="fields">
+                <p className="timeslot">8.00 am</p>
+                <p className="infoslot">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla deleniti, perferendis officiis impedit distinctio, quisquam asperiores assumenda in sunt earum inventore debitis explicabo consequuntur quidem reiciendis quo. Distinctio, fugit odit.
+                </p>
+            </div>
+            <div className="fields">
+                <p className="timeslot">8.00 am</p>
+                <p className="infoslot">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla deleniti, perferendis officiis impedit distinctio, quisquam asperiores assumenda in sunt earum inventore debitis explicabo consequuntur quidem reiciendis quo. Distinctio, fugit odit.
+                </p>
+            </div>
+            <div className="fields">
+                <p className="timeslot">8.00 am</p>
+                <p className="infoslot">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla deleniti, perferendis officiis impedit distinctio, quisquam asperiores assumenda in sunt earum inventore debitis explicabo consequuntur quidem reiciendis quo. Distinctio, fugit odit.
+                </p>
+            </div>
+            <div className="fields">
+                <p className="timeslot">8.00 am</p>
+                <p className="infoslot">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla deleniti, perferendis officiis impedit distinctio, quisquam asperiores assumenda in sunt earum inventore debitis explicabo consequuntur quidem reiciendis quo. Distinctio, fugit odit.
+                </p>
+            </div>
+            <div className="fields">
+                <p className="timeslot">8.00 am</p>
+                <p className="infoslot">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla deleniti, perferendis officiis impedit distinctio, quisquam asperiores assumenda in sunt earum inventore debitis explicabo consequuntur quidem reiciendis quo. Distinctio, fugit odit.
+                </p>
+            </div>
+            <div className="fields">
+                <p className="timeslot">8.00 am</p>
+                <p className="infoslot">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla deleniti, perferendis officiis impedit distinctio, quisquam asperiores assumenda in sunt earum inventore debitis explicabo consequuntur quidem reiciendis quo. Distinctio, fugit odit.
+                </p>
+            </div>
+        </div>
+    );
+};
 
 export default ScheduleField;
