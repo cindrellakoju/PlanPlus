4a56054878a71f091a310f859ad5f17d358a1585
cindrella koju <cindrellakoju@gmail.com>
2025-02-17 16:01:44 +0545

    set states in money trnsaction

--- src/components/money/MoneyField.tsx
+++ src/components/money/MoneyField.tsx
@@ -1,9 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
 const MoneyField:React.FC = () =>{
+    const [transactionstate,setTransactionState] = useState<boolean>(true)
     return(
-        <>
-        </>
+        <div>
+            <div>
+                <label htmlFor="money">Choose Transaction Option:</label>
+                <select id="money" onChange={()=> setTransactionState(!transactionstate)}>
+                    <option value="totake">To Take</option>
+                    <option value="togive">To Give</option>
+                </select>
+            </div>
+            <div>
+                {
+                    transactionstate ?
+                            <>
+                                <table>
+                                    <tr>
+                                        <th>Take Money From</th>
+                                        <th>Amount</th>
+                                        <th>Reason</th>
+                                    </tr>
+                                    <tr>
+                                        <td>Sanjib</td>
+                                        <td>Rs. 500</td>
+                                        <td>Chips of dharan</td>
+                                    </tr>
+                                </table>
+                            </>
+                    :
+                            <>
+                                <table>
+                                    <tr>
+                                        <th>Give Money To</th>
+                                        <th>Amount</th>
+                                        <th>Reason</th>
+                                    </tr>
+                                    <tr>
+                                        <td>Saurav</td>
+                                        <td>Rs. 500</td>
+                                        <td>Dharan totle</td>
+                                    </tr>
+                                </table>
+                            </>
+                }
+            </div>
+        </div>
     )
 }
 
