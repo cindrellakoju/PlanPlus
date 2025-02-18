0c2a2e72e5b4bec7ef52694c6f7c18fb4cf6e71e
cindrella koju <cindrellakoju@gmail.com>
2025-02-18 17:14:11 +0545

    Added clz fee and change  select option

--- src/components/money/MoneyField.tsx
+++ src/components/money/MoneyField.tsx
@@ -1,104 +1,130 @@
 import React, { useState } from "react";
 
-const MoneyField:React.FC = () =>{
-    const [transactionstate,setTransactionState] = useState<boolean>(true)
-    return(
+const MoneyField: React.FC = () => {
+    const [transactionstate, setTransactionState] = useState<string>("totake");
+    const handleTransactionChange = (event : React.ChangeEvent<HTMLSelectElement>) =>{
+        setTransactionState(event.target.value)
+    }
+    return (
         <div className="moneyfieldcontainer">
             <div className="chooseoption">
                 <label htmlFor="money">Choose Transaction Option:  </label>
-                <select id="money" onChange={()=> setTransactionState(!transactionstate)}>
+                <select id="money" onChange={handleTransactionChange}>
                     <option value="totake">To Take</option>
                     <option value="togive">To Give</option>
+                    <option value="clzfee">Clz Fee</option>
                 </select>
             </div>
             <div className="tablefield">
-                {
-                    transactionstate ?
-                            <>
-                                <table>
-                                    <thead>
-                                        <tr>
-                                            <th style={{width:"30%"}}>Take Money From</th>
-                                            <th style={{width:"10%"}}>Amount</th>
-                                            <th style={{width:"50%"}}>Reason</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {/* <tr>
-                                            <td>Sanjib</td>
-                                            <td>Rs. 500</td>
-                                            <td>Chips of dharansdhisb scss sdhj</td>
-                                        </tr> */}
-                                        <tr>
-                                            <td>
-                                                <input
-                                                type="text"
-                                                placeholder="Take Money From..."
-                                                id="moneyfrom"
-                                                className="input-field"
-                                                />
-                                            </td>
-                                            <td>
-                                                <input
-                                                type="text"
-                                                placeholder="Amount..."
-                                                className="input-field"
-                                                />
-                                            </td>
-                                            <td>
-                                                <input
-                                                type="text"
-                                                placeholder="Reason..."
-                                                className="input-field"
-                                                />
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </>
-                    :
-                            <>
-                                <table>
-                                    <tr>
-                                        <th style={{width:"30%"}}>Give Money To</th>
-                                        <th style={{ width:"10%"}}>Amount</th>
-                                        <th style={{width:"50%"}}>Reason</th>
-                                    </tr>
-                                    {/* <tr>
-                                        <td>Saurav</td>
-                                        <td>Rs. 500</td>
-                                        <td>Dharan totle</td>
-                                    </tr> */}
-                                    <tr>
-                                        <td>
-                                            <input
+                {transactionstate === "totake" ? (
+                    <>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th style={{ width: "30%" }}>Take Money From</th>
+                                    <th style={{ width: "10%" }}>Amount</th>
+                                    <th style={{ width: "50%" }}>Reason</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <input
                                             type="text"
-                                            placeholder="Give Money To..."
+                                            placeholder="Take Money From..."
                                             id="moneyfrom"
                                             className="input-field"
                                         />
-                                        </td>
-                                        <td>
-                                            <input
+                                    </td>
+                                    <td>
+                                        <input
                                             type="text"
                                             placeholder="Amount..."
                                             className="input-field"
-                                            />
-                                        </td>
-                                        <td>
-                                            <input
+                                        />
+                                    </td>
+                                    <td>
+                                        <input
                                             type="text"
                                             placeholder="Reason..."
                                             className="input-field"
                                         />
-                                        </td>
-                                    </tr>
-                                </table>
-                            </>
-                }
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </>
+                ) : ( transactionstate === "togive" ?
+                    <>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th style={{ width: "30%" }}>Give Money To</th>
+                                    <th style={{ width: "10%" }}>Amount</th>
+                                    <th style={{ width: "50%" }}>Reason</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <input
+                                            type="text"
+                                            placeholder="Give Money To..."
+                                            id="giveto"
+                                            className="input-field"
+                                        />
+                                    </td>
+                                    <td>
+                                        <input
+                                            type="text"
+                                            placeholder="Amount..."
+                                            className="input-field"
+                                        />
+                                    </td>
+                                    <td>
+                                        <input
+                                            type="text"
+                                            placeholder="Reason..."
+                                            className="input-field"
+                                        />
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </>:
+                    <>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th style={{ width: "30%" }}>Paid Amount</th>
+                                    <th style={{ width: "50%" }}>Reason</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <input
+                                            type="text"
+                                            placeholder="Enter Paid Amount"
+                                            id="paidamount"
+                                            className="input-field"
+                                        />
+                                    </td>
+                                    <td>
+                                        <input
+                                            type="text"
+                                            placeholder="Reason..."
+                                            className="input-field"
+                                        />
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </>
+                )}
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default MoneyField;
