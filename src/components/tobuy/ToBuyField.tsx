78eb2f867f3aa42167c6faaaf2714a66bdabda8f
cindrella koju <cindrellakoju@gmail.com>
2025-02-21 19:16:18 +0545

    buylist

--- src/components/tobuy/ToBuyField.tsx
+++ src/components/tobuy/ToBuyField.tsx
@@ -2,8 +2,32 @@ import React from "react";
 
 const ToBuyField : React.FC = () =>{
     return(
-        <>
-        </>
+        <div className="buylistfieldcontainer">
+            <div className="buylistfield">
+                <div className="field">
+                    <input type="checkbox" id="buylist1" name="buylist1" value="buylist" />
+                    <label htmlFor="buylist">gaming pc</label>
+                </div>
+                <div className="field">
+                    <input type="checkbox" id="buylist1" name="buylist1" value="buylist" />
+                    <label htmlFor="buylist">gaming pc</label>
+                </div>
+                <div className="field">
+                    <input type="checkbox" id="buylist1" name="buylist1" value="buylist" />
+                    <label htmlFor="buylist">gaming pc</label>
+                </div>
+                <div className="field">
+                    <input type="checkbox" id="buylist1" name="buylist1" value="buylist" />
+                    <label htmlFor="buylist">gaming pc</label>
+                </div>
+                <div className="field">
+                    <input type="checkbox" id="buylist1" name="buylist1" value="buylist" />
+                    <label htmlFor="buylist">gaming pc</label>
+                </div>
+
+            </div>
+
+        </div>
     )
 }
 
