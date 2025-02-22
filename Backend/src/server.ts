c47ccb97d1a856f10a710e0f566f369b67fbb4bc
cindrella koju <cindrellakoju@gmail.com>
2025-02-22 19:45:26 +0545

    added routes and updated server.ts

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,10 +1,15 @@
 import express from 'express';
 import dotenv from "dotenv";
 import cors from "cors";
+import userRoutes from "./routes/userRoutes";
 
 dotenv.config();
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use("/users",userRoutes)
 
 const PORT = process.env.DB_PORT;
 app.listen(PORT,()=>
