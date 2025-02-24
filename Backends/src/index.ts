ebb7eacb3d00264a7531759bbec81cdcaa5c2cbd
cindrella koju <cindrellakoju@gmail.com>
2025-02-24 11:49:49 +0545

    add cors because backend is not allowing requests

--- Backends/src/index.ts
+++ Backends/src/index.ts
@@ -1,11 +1,16 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import router from "./routes/user.routes";
+import cors from "cors"
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+// Use CORS with options
+app.use(cors({
+  origin: 'http://localhost:5173', // Allow only your frontend to access
+}));
 
 app.use(express.json());
 app.use("/user",router);
