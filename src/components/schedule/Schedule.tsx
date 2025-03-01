0b450c3f5a84ec8c29354314a8fa3f70145b0a8e
cindrella koju <cindrellakoju@gmail.com>
2025-03-01 14:13:48 +0545

    Schdeulepge done

--- src/components/schedule/Schedule.tsx
+++ src/components/schedule/Schedule.tsx
@@ -2,10 +2,12 @@ import React from "react"
 import { today_day_long } from "../../utils/formatDate"
 import ScheduleField from "./ScheduleField"
 import "../../styles/Schedule.css"
+import ComponentWidthHeight from "../../utils/ComponentWidthHeight"
 
 const Schedule:React.FC= () =>{
+    const {max_height,max_width}=ComponentWidthHeight("Schedule")
     return(
-        <div className="schedulecontainer">
+        <div className="schedulecontainer" style={{ maxHeight: max_height, width: max_width}}>
             <h1>Schedule for {today_day_long}</h1>
             <ScheduleField/>
         </div>
