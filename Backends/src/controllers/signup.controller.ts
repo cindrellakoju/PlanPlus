7b51139cb148686342e3653008bc9929ebc0bc4f
cindrella koju <cindrellakoju@gmail.com>
2025-03-12 20:21:56 +0545

    removed consoles

--- Backends/src/controllers/signup.controller.ts
+++ Backends/src/controllers/signup.controller.ts
@@ -25,9 +25,7 @@ export const signup = async(req:Request, res:Response):Promise<void> => {
 
 export const login = async (req: Request, res: Response): Promise<void> => {
     try {
-        console.log(req.body)
         const result = await loginUser(req.body);
-        console.log(result)
         res.status(200).json({ message: "Login successful" });
     } catch (err) {
         res.status(500).json({ message: "Error fetching login info", error: err });
