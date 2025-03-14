851448e9654fc5f9737be6b9d77cb0d398b424b6
cindrella koju <cindrellakoju@gmail.com>
2025-03-14 14:32:26 +0545

    frontend for login

--- src/components/notLogin/LoginField.tsx
+++ src/components/notLogin/LoginField.tsx
@@ -1,13 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
 
-const LoginField: React.FC = () => {
-  return (
-    <div className='loginfield'>
-        <input type='text' id='user' placeholder='Enter your username'/>
-        <input type='password' id='password' placeholder='Enter Your Password'/>
-        <button>Send</button>
-    </div>
-  )
+interface LoginInfo {
+  email: string;
+  password: string;
 }
 
-export default LoginField
+const LoginField: React.FC = () => {
+  const backend_url = import.meta.env.VITE_BACKEND_URL;
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [loginInfo, setLoginInfo] = useState<LoginInfo | null>(null); // Initialize as null
+
+  const handleLogin = () => {
+    // Basic form validation before sending request
+    if (!email || !password) {
+      console.log('Email and Password are required.');
+      return;
+    }
+
+    const logInfo: LoginInfo = {
+      email,
+      password,
+    };
+
+    setLoginInfo(logInfo); // Optional: keep track of the login data
+
+    axios
+      .post(`${backend_url}/user/login`, logInfo)
+      .then((response) => {
+        console.log('Login successful:', response.data);
+        // Optionally clear the password after a successful login for security purposes
+        setPassword('');
+      })
+      .catch((err) => {
+        console.error('Error login:', err);
+      });
+  };
+
+  return (
+    <div className="loginfield">
+      <input
+        type="text"
+        id="user"
+        placeholder="Enter your username"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        id="password"
+        placeholder="Enter Your Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+};
+
+export default LoginField;
