164e5d8a0b789bbbebccff1d36c75d43484da153
cindrella koju <cindrellakoju@gmail.com>
2025-03-25 12:43:06 +0545

    fetched the component in / page

--- src/components/allrequire/SideBar.tsx
+++ src/components/allrequire/SideBar.tsx
@@ -1,25 +1,9 @@
-import axios from "axios";
 import "../../styles/SideBar.css";
-import { useContext, useEffect, useState } from "react";
+import { useContext} from "react";
 import { MyContext } from "../../context/Component.context";
 import { ComponentType } from "../../types";
 
 const SideBar: React.FC = () => {
-  const backend_url = import.meta.env.VITE_BACKEND_URL;
-
-
-  const storedUser = localStorage.getItem('userdetail');
-    const [userid, setUserId] = useState<{ user_id:string} | null>(null);
-  
-    useEffect(() => {
-      if (storedUser) {
-        const parsedUser = JSON.parse(storedUser);
-        setUserId(parsedUser); // Store parsed user info in state
-      }
-    }, [storedUser]);
-
-    const userId = userid?.user_id
-    console.log("User ID:",userId)
   // Use context
   const context = useContext(MyContext);
 
@@ -27,22 +11,6 @@ const SideBar: React.FC = () => {
   if (!context) {
     throw new Error("SideBar must be used within a MyProvider");
   }
-
-  console.log(`${backend_url}/user/tablename/${userId}`)
-  // Fetch components data from the API on mount
-  useEffect(() => {
-    if(userId){
-    axios
-      .get(`${backend_url}/user/tablename/${userId}`) // API endpoint for components
-      .then((response) => {
-        console.log("Successfully fetched components: ", response.data);
-        context.setComponents(response.data); // Set the fetched components to context
-      })
-      .catch((error) => {
-        console.error("Error fetching the data: ", error); // Handle any errors
-      });
-    }
-  }, [userId]);
 
   // Handle component selection
   const handleSelectComponent = (component: ComponentType) => {
