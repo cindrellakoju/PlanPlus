4299b20c44a57959fcf352dc6c41b25592efb535
Cindrella Koju <cindrellakoju@gmail.com>
2025-04-25 19:01:33 +0545

    date and day

--- src/components/calender/datainfo.tsx
+++ src/components/calender/datainfo.tsx
@@ -4,6 +4,9 @@ const DataInfo:React.FC = () => {
     return(
        <>
             <div className="datafield">
+                <div className="dayfield">
+                    <h1>Tuesday(April 10,2025)</h1>
+                </div>
                 <div className="eachdatainfo">
                     <div className="colname">
                         <div className="tag">Task</div>
